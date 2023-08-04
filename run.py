e657834dafa12072040809d0f1aac28c37bf0584
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-08-04 11:31:28 +0000

    Added validate_data_name function for name input to check for numbers and symbols in input

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('save_our_library')
 
+
+
 """
 def welcome():
     Welcome page Save our Library
@@ -24,17 +26,26 @@ def options():
     Option 3 - Exit
 """
 
+
 def get_name_data():
     """
     Get name input from user
     """
+    data_name = " "
+
     print("Lets start by telling us who this donation is from...")
     print("You can also make this anonymous, simply type Anonymous\n")
-    
     data_name = input("Enter your name here: ")
+
     validate_name_data(data_name)
 
-def validate_name_data(get_name_data):
- 
+def validate_name_data(data_name):
 
+    if any(char.isdigit() for char in data_name):
+        print("Error: Name cannot contain numbers or symbols.")
+    elif data_name.isalnum():
+        print("Error: Name cannot contain numbers or symbols.")
+    elif type(data_name) == str:
+        print(f"Thank you, this Donation is from {data_name}")
+        
 get_name_data()
